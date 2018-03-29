ff481673ec361824feb375de1adb078d30eb5c5d
David-pc <davidmaccurso@gmail.com>
2018-03-29 00:43:45 -0300

    29/03
    Terminado el mapa. Comienzo de jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+function initMap() {
+  var latLng = {
+    lat: -32.9284726,
+    lng: -60.7279232
+  };
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('mapa'), {
+    //center: {lat: -32.9284726, lng: -60.7279232},
+    center: latLng,
+    zoom: 13,
+    'mapTypeId': google.maps.MapTypeId.ROADMAP
+  });
+  var contenido = '<h2>GDLWEBCAMP</H2>' +
+                   '<p>Del 10 al 12 de Abril</p>' +
+                   '<p>Visitanos!</p>' +
+                   '<p>Cordoba y Circunvalacion, Rosario, Sta Fe, Arg.</p>'
+  var info = new google.maps.InfoWindow({
+    content: contenido
+  });
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: 'Ubicacion'
+  });
+  marker.addListener('click', function(){
+    info.open(map, marker);
+  });
+}
+
 (function(){
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
@@ -25,6 +54,36 @@
   pase_dia.addEventListener('blur', mostrarDias);
   pase_dos_dias.addEventListener('blur',mostrarDias);
   pase_completo.addEventListener('blur',mostrarDias);
+  nombre.addEventListener('blur', validarCampos);
+  apellido.addEventListener('blur', validarCampos);
+  email.addEventListener('blur', validarCampos);
+  email.addEventListener('blur', validarEmail);
+
+  function validarEmail() {
+    if(this.value.indexOf("@") > -1 ) {
+      errorDiv.style.display = 'none';
+      this.style.border = '1px solid #e1e1e1';
+    }
+    else {
+      errorDiv.style.display = 'block';
+      errorDiv.innerHTML = "Debe tener formato de Email";
+      this.style.border = '1px solid red';
+      errorDiv.style.border = '1px solid red';
+    }
+  }
+  function validarCampos()
+  {
+    if (this.value == '') {
+    errorDiv.style.display = 'block';
+    errorDiv.innerHTML = "Este campo es obligatorio";
+    this.style.border = '1px solid red';
+    errorDiv.style.border = '1px solid red';
+    }
+    else {
+      errorDiv.style.display = 'none';
+      this.style.border = '1px solid #e1e1e1';
+    }
+ }
 
   function mostrarDias(){
       var boletosDia = parseInt(pase_dia.value,10) || 0,
@@ -49,6 +108,7 @@
         document.getElementById(diasElegidos[i]).style.display = 'block';
       }
     }
+
   function calcularMontos(event) {
     event.preventDefault(); //Previene accion por defecto. Ej link no abre pag
     if (regalo.value === '') {
