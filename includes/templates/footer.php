606347bd0371780881d1b8bc8ac6623a3c85510e
DavidAccurso <davidmaccurso@gmail.com>
2018-05-03 00:25:32 -0300

    header y footer en php
    Index y Registro se pasaron a php y se creo el header y footer para reutilizar ese codigo

---
+++ includes/templates/footer.php
@@ -0,0 +1,49 @@
+      <footer class="site-footer">
+        <div class="contenedor clearfix">
+          <div class="footer-informacion">
+            <h3>Sobre <span>gldwebcamp</span> </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <div class="ultimos-tweets">
+            <h3>Ultimos <span>tweets</span> </h3>
+            <ul>
+              <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+              <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+              <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+            </ul>
+          </div>
+          <div class="menu">
+            <h3>Redes <span>sociales</span> </h3>
+            <nav class="redes-sociales">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-pinterest"></i></a>
+              <a href="#"><i class="fab fa-youtube"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+            </nav> <!-- nav.redes-sociales -->
+          </div>
+        </div>
+        <p class="copyright">Todos los derechos Reservados GDLWEBCAMP 2016.</p>
+      </footer>
+
+      <!-- Codigo generado por html5-boilerplate -->
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js\PluginAnimacion.js"></script>
+        <script src="js\CountDown.js"></script>
+        <script src="js\jquery.lettering.js"></script>
+        <script src="js\jquery.waypoints.js"></script>
+        <script src="js/main.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAc6M-e4Wqzmt01R3dXot6J928B69z2078&callback=initMap"
+        async defer></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+
+      <header class="site-header">
+        <div class="hero">
+          <div class="contenido-header">
+            <nav class="redes-sociales">
+              <a href="http://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+              <a href="http://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+              <a href="http://www.pinterest.com" target="_blank"><i class="fab fa-pinterest"></i></a>
+              <a href="http://www.youtube.com" target="_blank"><i class="fab fa-youtube"></i></a>
+              <a href="http://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+            </nav> <!-- nav.redes-sociales -->
+            <div class="informacion-evento clearfix">
+              <div class="clearfix">
+                <p class="fecha"><i class="far fa-calendar-alt"></i>10-12 Abril</p>
+                <p class="ciudad"><i class="fas fa-map-marker-alt"></i>Guadalajara, MX</p>
+              </div> <!-- .clearfix -->
+              <h1 class="nombre-sitio">GdlWebCamp</h1>
+              <p class="slogan">La mejor conferencia de <span>Diseño Web</span> </p>
+            </div> <!-- .informacion-evento -->
+          </div> <!-- -contenido-header -->
+        </div> <!-- .hero -->
+      </header>
+
+      <div class="barra">
+        <div class="contenedor clearfix">
+          <div class="logo">
+            <a href="index.php">
+              <img src="img/logo.svg" alt="imagen logo">
+            </a>
+          </div> <!-- .logo -->
+          <div class="menu-movi">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div> <!-- .menu-movi -->
+          <nav class="navegacion-principal clearfix">
+            <a href="conferencia.html">Conferencia</a>
+            <a href="#">Calendario</a>
+            <a href="#">Invitados</a>
+            <a href="registro.php">Reservaciones</a>
+          </nav>
+        </div> <!-- .contenedor -->
+      </div> <!-- .barra -->
+      
